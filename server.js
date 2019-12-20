b9ae324279616acefe25042dc40b1436eeba34b8
unknown <alexandresilvadj@hotmail.com>
2019-12-19 21:07:14 -0300

    Criação do website fixed #4

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 
+app.set('view engine', 'pug');
+
 app.get('/', (req, res) => {
-    res.send('Hello World!');
-  });
+    res.render('index', {title: 'Homepage'
+});
 
 const server = app.listen(7000, () => {
     console.log(`Express running → PORT ${server.address().port}`);
