e2bba0b317fbf641f7f4f3ec75200dcf4d3e9e83
jonilsons9 <jonilsons9@gmail.com>
2019-12-20 13:29:52 -0300

    trabalhando com json completo fixed #6

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 const express = require('express');
+const people = require('./people.json');
+
 const app = express();
 
 app.set('view engine', 'pug');
 
-// serve static files from the `public` folder
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-    res.render('index', {
-    title: 'Homepage'
-    });
+  res.render('index', {
+    title: 'Homepage',
+    people: people.profiles
+  });
 });
 
 const server = app.listen(7000, () => {
-    console.log(`Express running → PORT ${server.address().port}`);
-  });
+  console.log(`Express running → PORT ${server.address().port}`);
+});
